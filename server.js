569bfad5610903617037af0b4a2b337f06e0206f
ITbrandon <brandoniticka@gmail.com>
2025-03-17 18:23:52 -0600

    added cors

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const characters = require('./characters.json')
 const path = require('path');
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
