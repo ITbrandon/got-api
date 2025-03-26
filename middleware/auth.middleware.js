4a73771732198a7a719bb77444811309ef56a917
ITbrandon <brandoniticka@gmail.com>
2025-03-25 19:55:03 -0600

    updated middleware

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -7,9 +7,15 @@ export const authenticateUser = (req, res, next) => {
     if (!token) {
         return res.status(401).json({ message: "Access denied. No token provided." });
     }
+    
+    const tokenPart = token.split(" ")[1];
+
+    if (!tokenPart) {
+        return res.status(400).json({ message: "Invalid token format" });
+    }
 
     try {
-        const verified = jwt.verify(token, process.env.JWT_SECRET);
+        const verified = jwt.verify(tokenPart, process.env.JWT_SECRET);
         req.user = verified;
         next(); 
     } catch (error) {
