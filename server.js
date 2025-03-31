d928cf9c37e9ecf19e56d9f6451cf6da524a9b45
ITbrandon <brandoniticka@gmail.com>
2025-03-31 09:41:42 -0600

    added credentials

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ app.use(express.urlencoded({extended: false}));
 app.use(cors({
   origin: 'http://localhost:5173',
   methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"]
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
 }));
 
 const __filename = fileURLToPath(import.meta.url);
