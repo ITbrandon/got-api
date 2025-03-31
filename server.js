1baa1b3f7878e2cfa47e85685a80424f30f84bb5
ITbrandon <brandoniticka@gmail.com>
2025-03-31 09:34:42 -0600

    fixed cors origin

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cors({
-  origin: '*',
+  origin: 'http://localhost:5173',
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
